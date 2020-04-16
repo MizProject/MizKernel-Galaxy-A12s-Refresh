7e0c264b3a00ffd88babdc65911c8084b312e0d8
Suren Baghdasaryan <surenb@google.com>
2020-04-16 13:06:50 -0700

    ANDROID: GKI: fix ABI diffs caused by ION heap and pool vmstat additions
    New nr_ion_heap and nr_ion_heap_pool fields in vmstat file cause ABI
    differences. Fix them by adding the fields.
    
    Bug: 110330255
    Bug: 130198686
    Bug: 153442668
    Test: boot
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 8f76a3ed2a76492deba9edebc990cac3b32f78be)
    Signed-off-by: Martin Liu <liumartin@google.com>
    [surenb: cherry picked and trimmed the original patch to include only
    necessary changes to resolve ABI diff for node_stat_item enum]
    
    Bug: 149182139
    Test: build and boot
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I82b43bd3d94b950bd266aa53a3794182604a52d0

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -187,6 +187,9 @@ enum node_stat_item {
 	NR_DIRTIED,		/* page dirtyings since bootup */
 	NR_WRITTEN,		/* page writings since bootup */
 	NR_KERNEL_MISC_RECLAIMABLE,	/* reclaimable non-slab kernel pages */
+	NR_UNRECLAIMABLE_PAGES,
+	NR_ION_HEAP,
+	NR_ION_HEAP_POOL,
 	NR_VM_NODE_STAT_ITEMS
 };
 
